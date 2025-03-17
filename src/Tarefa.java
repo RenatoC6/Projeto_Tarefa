ccfdd978d72ead3d52becb69a0715aa7a1f9eac4
campi <campionr6@gmail.com>
2025-03-16 22:07:04 -0300

    commit 16_03

---
+++ src/Tarefa.java
@@ -0,0 +1,48 @@
+import java.time.LocalDate;
+
+public class Tarefa {
+
+    private String titulo;
+    private String descricao;
+    private LocalDate dataLimite;
+    private StatusTarefa statusTarefa;
+
+    public Tarefa(String titulo, String descricao, LocalDate dataLimite, StatusTarefa statusTarefa) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataLimite = dataLimite;
+        this.statusTarefa = statusTarefa;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataLimite() {
+        return dataLimite;
+    }
+
+    public void setDataLimite(LocalDate dataLimite) {
+        this.dataLimite = dataLimite;
+    }
+
+    public StatusTarefa getStatusTarefa() {
+        return statusTarefa;
+    }
+
+    public void setStatusTarefa(StatusTarefa statusTarefa) {
+        this.statusTarefa = statusTarefa;
+    }
+}
